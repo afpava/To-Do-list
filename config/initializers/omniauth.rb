6bd988bd7807556eebc7f5d54611314c53073a8c
Pasha Afanasev <afanasev_p@ukr.net>
2018-12-31 01:49:46 +0200

    make it better

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, "754802324470-7poiqgk8c2j0of9mtutg5s9t7i0in6lv.apps.googleusercontent.com", "iZFvmtcQzTnm4WsRRI1uDRi8"
+  provider :google_oauth2, "754802324470-7poiqgk8c2j0of9mtutg5s9t7i0in6lv.apps.googleusercontent.com", "iZFvmtcQzTnm4WsRRI1uDRi8", {access_type: 'online'}
 end
